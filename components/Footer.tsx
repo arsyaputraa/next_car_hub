1cd69bc9cdc1cde82bec236c85ff8c71141b9877
Arsya Putra <73641300+arsyaputraa@users.noreply.github.com>
2024-03-06 14:59:51 +0700

    fixing footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,7 +22,10 @@ const Footer = () => {
         </div>
         <div className="flex-1 w-full flex md:justify-end flex-wrap max-md:mt-10 gap-20">
           {footerLinks.map((link: FooterLink) => (
-            <div className="flex flex-col gap-6 text-base min-w-[170px]">
+            <div
+              key={link.title}
+              className="flex flex-col gap-6 text-base min-w-[170px]"
+            >
               <h3 className="font-bold">{link.title}</h3>
               {link.links.map((item) => (
                 <Link
